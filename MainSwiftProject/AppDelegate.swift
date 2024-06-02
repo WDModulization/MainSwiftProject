1648806fd9ed4617bcc76ea25745d68fcae04a98
scott <>
2024-06-02 18:23:30 +0800

    initialization

---
+++ MainSwiftProject/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MainSwiftProject
+//
+//  Created by scott on 2024/6/2.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ MainSwiftProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MainSwiftProject
+//
+//  Created by scott on 2024/6/2.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

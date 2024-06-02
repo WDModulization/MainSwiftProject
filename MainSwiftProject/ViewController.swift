7c5f523f122cbc169696e464cbaced0bd532e3d3
scott <>
2024-06-02 18:39:20 +0800

    add podfile

--- MainSwiftProject/ViewController.swift
+++ MainSwiftProject/ViewController.swift
@@ -5,20 +5,13 @@
 //  Created by scott on 2024/6/2.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
--- README.md
+++
@@ -1,2 +0,0 @@
-# MainSwiftProject
-一个纯swift的组件化demoApp，开发组件全部使用本地pod
